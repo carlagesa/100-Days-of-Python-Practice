9bbb8e3c08fad15b034c670c8b80717ce2d7edad
carlagesa <carltonmbaja0@gmail.com>
2023-01-04 20:06:22 +0300

    Added photo input with billing

--- rollercoaster.py
+++ rollercoaster.py
@@ -1,13 +1,24 @@
 print('Welcome to our rollercoaster!')
 height = int(input('What is your height?cm'))
+bill = 0
 if height >= 120:
     print('Climb aboard mate!')
     age = int(input("What is your age?"))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Children tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo take? Y or N.")
+    if wants_photo == "Y":
+        print(f"Your total bill is ${bill + 3}")
+    else:
+        print(f"Your total bill is ${bill}.")
+
 else:
-    print('Barnicles! Me little shortstick cannot ride')
+    print('Barnacles! Me little shortstick cannot ride')
